ea444b28ecdb7c99fb2427f0dfd17e02ee9c2c19
shihaoyan <shihaoyan0209@163.com>
2020-11-03 09:15:43 +0800

    项目搭建

--- src/com/shy/jvm/Main.java
+++ src/com/shy/jvm/Main.java
@@ -1,27 +1,14 @@
 package com.shy.jvm;
 
-import java.util.Arrays;
-
 /**
  * @author 石皓岩
  * @date 2020/10/13 16:58
- * 描述:
+ * 描述:全局主类，一般不会用到。
  */
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(StrToBinstr("Save"));
+    }
 
-    }
-    private static String StrToBinstr(String str) {
-        char[] strChar=str.toCharArray();
-        byte[] bytes = str.getBytes();
-        System.out.println(Arrays.toString(bytes));
-        String result="";
-        for(int i=0;i<strChar.length;i++){
-            result +=Integer.toBinaryString(strChar[i])+ " ";
-        }
-        return result;
-    }
 }
